c372f8982a63535fa5a5af6a6f3bc573ae1a4167
barry.paneer <barry.paneer@qq.com>
2017-10-23 18:37:44 +0800

    updates

--- DataCollector4CTPDL/CTPConnection/CTPQuotation.cpp
+++ DataCollector4CTPDL/CTPConnection/CTPQuotation.cpp
@@ -620,12 +620,12 @@ void CTPQuotation::FlushQuotation( CThostFtdcDepthMarketDataField* pQuotationDat
 	{	///< 更新日期+时间
 		tagStatus.MarketStatus = 1;
 		tagStatus.MarketTime = nSnapUpdateTime;
-		QuoCollector::GetCollector()->OnData( 102, (char*)&tagStatus, sizeof(tagStatus), false );
+		QuoCollector::GetCollector()->OnData( 102, (char*)&tagStatus, sizeof(tagStatus), true );
 	}
 
-	QuoCollector::GetCollector()->OnData( 104, (char*)&tagSnapLF, sizeof(tagSnapLF), false );
-	QuoCollector::GetCollector()->OnData( 105, (char*)&tagSnapHF, sizeof(tagSnapHF), false );
-	QuoCollector::GetCollector()->OnData( 106, (char*)&tagSnapBS, sizeof(tagSnapBS), false );
+	QuoCollector::GetCollector()->OnData( 104, (char*)&tagSnapLF, sizeof(tagSnapLF), true );
+	QuoCollector::GetCollector()->OnData( 105, (char*)&tagSnapHF, sizeof(tagSnapHF), true );
+	QuoCollector::GetCollector()->OnData( 106, (char*)&tagSnapBS, sizeof(tagSnapBS), true );
 }
 
 void CTPQuotation::OnRspError( CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast )
